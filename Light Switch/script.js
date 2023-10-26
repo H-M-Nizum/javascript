990dedc7f1649aac42ac875002f66abaaf2523b5
H-M-Nizum <hmnizum1714032@gmail.com>
2023-10-26 14:35:58 +0600

    Switch

---
+++ Light Switch/script.js
@@ -0,0 +1,11 @@
+function greenLight() {
+  document.getElementById("myImage").src = "Green_light.png";
+}
+
+function redLight() {
+  document.getElementById("myImage").src = "red_lightt.jpg";
+}
+
+function lightOFF() {
+  document.getElementById("myImage").src = "off_light.png";
+}
